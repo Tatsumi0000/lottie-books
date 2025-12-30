8f87b78eeef84dac14cf7a605524a47a5ddb13e2
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2025-12-31 00:21:08 +0900

    pluginは種類ごとにファイルに分けた方がわかりやすいので修正

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,19 @@
+import "unfonts.css";
+import '@mdi/font/css/materialdesignicons.css'
+// @ts-ignore "vuetify/styles.css"にしないとエラーにならないけど拡張子をつけるとビルドエラーになるので除外
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives"
+
+export default createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: "dark"
+  },
+  icons: {
+    defaultSet: 'mdi'
+  },
+});
+
