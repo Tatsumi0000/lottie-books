a54aca01696cdece1dabed45a5a22f60e3ea739f
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2026-01-14 01:06:24 +0900

    動的ルーティングがgithub pagesだと正しく動かないのでハッシュモードで動かす

--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 import HelloWorld from "@/components/HelloWorld.vue";
 import Top from "@/layouts/Top.vue";
@@ -18,6 +18,6 @@ const routes = [
 ];
 
 export const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 });
