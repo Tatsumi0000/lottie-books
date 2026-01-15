d6978fadd9375e8f624e07f5347542ea17325ec7
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2026-01-15 22:38:27 +0900

    フッターを追加

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import TheHeader from "@/components/TheHeader.vue";
+import TheFooter from "./components/TheFooter.vue";
 </script>
 
 <template>
@@ -8,6 +9,7 @@ import TheHeader from "@/components/TheHeader.vue";
     <VMain>
       <RouterView />
     </VMain>
+    <TheFooter />
   </VApp>
 </template>
 
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <VFooter
+    class="align-center justify-center"
+    color="surface-light"
+    density="compact"
+  >
+    {{ new Date().getFullYear() }} — Created by Tatsumi0000
+  </VFooter>
+</template>
+
+<style scoped></style>
